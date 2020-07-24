2d310a491c0f738c4755b38167814584ffa61785
liucanbin <liucanbin@cvte.com>
2020-07-24 15:01:28 +0800

    Commander.js 命令熟悉

--- dist/main.js
+++ dist/main.js
@@ -24,18 +24,26 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // console.log(process.argv);
 
-_commander2.default.usage('<command> [options]');
+_commander2.default.usage("<command> [options]");
 
-_commander2.default.command('create').description('创建一个项目').alias('c').action(function () {
-  _create2.default.apply(undefined, (0, _toConsumableArray3.default)(process.argv.slice(3)));
+_commander2.default.command("create").description("创建一个项目").alias("c").usage("<command> <project>").action(function () {
+	_create2.default.apply(undefined, (0, _toConsumableArray3.default)(process.argv.slice(3)));
 });
 
-//ommand第一个参数为命令名称，alias为命令的别称， 其中<>包裹的为必选参数 []为选填参数 带有...的参数为剩余参数的集合
-_commander2.default.command('init <type> [name] [otherParams...]').alias('i').description('Generates new code').action(function (type, name, otherParams) {
-  console.log('type', type);
-  console.log('name', name);
-  console.log('other', otherParams);
-  // 在这里执行具体的操作
+//command方法在接受不同数量的参数时会返回不同的对象，只接受一个参数子命令名的话返回的是 子命令本身，而如果加上第二个参数子命令介绍的话，返回值就变成了整个命令行的原型
+
+//command第一个参数为命令名称，alias为命令的别称， 其中<>包裹的为必选参数 []为选填参数 带有...的参数为剩余参数的集合
+_commander2.default.command("init <type> [name] [otherParams...]").alias("i").description("Generates new code")
+// .option('-n, --name <items1> [items2]', 'name description', 'default value')
+.option("-d, --day [d]").action(function (type, name, otherParams, cmd) {
+	console.log("type", type);
+	console.log("name", name);
+	console.log("other", otherParams);
+	console.log("day", cmd.day);
+	// 在这里执行具体的操作
 });
 
-_commander2.default.version(require('../package.json').version, '-v --version').parse(process.argv);
+//子命令模式，
+_commander2.default.command("test", "test");
+
+_commander2.default.version(require("../package.json").version, "-v --version").parse(process.argv);
--- src/main.js
+++ src/main.js
@@ -2,33 +2,39 @@ import program from "commander";
 import symbol from "log-symbols";
 import chalk from "chalk";
 
-import create from './create';
+import create from "./create";
 
 // console.log(process.argv);
 
-program.usage('<command> [options]')
+program.usage("<command> [options]");
 
 program
-    .command('create')
-    .description('创建一个项目')
-    .alias('c')
-    .action(()=>{
-      create(...process.argv.slice(3))
-    });
+	.command("create")
+	.description("创建一个项目")
+	.alias("c")
+	.usage("<command> <project>")
+	.action(() => {
+		create(...process.argv.slice(3));
+	});
+
+//command方法在接受不同数量的参数时会返回不同的对象，只接受一个参数子命令名的话返回的是 子命令本身，而如果加上第二个参数子命令介绍的话，返回值就变成了整个命令行的原型
 
 //command第一个参数为命令名称，alias为命令的别称， 其中<>包裹的为必选参数 []为选填参数 带有...的参数为剩余参数的集合
 program
-    .command('init <type> [name] [otherParams...]')
-    .alias('i')
-    .description('Generates new code')
-    .action(function (type, name, otherParams) {
-      console.log('type', type);
-      console.log('name', name);
-      console.log('other', otherParams);
-      // 在这里执行具体的操作
-    });
-  
+	.command("init <type> [name] [otherParams...]")
+	.alias("i")
+	.description("Generates new code")
+	// .option('-n, --name <items1> [items2]', 'name description', 'default value')
+	.option("-d, --day [d]")
+	.action(function (type, name, otherParams, cmd) {
+		console.log("type", type);
+		console.log("name", name);
+		console.log("other", otherParams);
+		console.log("day", cmd.day);
+		// 在这里执行具体的操作
+	});
 
-program
-    .version(require('../package.json').version, '-v --version')
-    .parse(process.argv);
+//子命令模式，
+program.command("test", "test");
+
+program.version(require("../package.json").version, "-v --version").parse(process.argv);
