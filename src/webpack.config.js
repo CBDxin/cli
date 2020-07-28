fc12b04c6696ce77a47b19791ba78d99e91cf122
liucanbin <liucanbin@cvte.com>
2020-07-28 12:11:47 +0800

    [feat]增加webpack基本配置

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,23 +1,30 @@
 "use strict";
 
 const { join } = require("path");
-const webpack = require("webpack");
+// const webpack = require("webpack");
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const isDev = process.env.NODE_ENV !== "production";
 
-module.exports = {
-	// entry: resolve(__dirname, "./entry.js"),
-	entry: ["webpack-hot-middleware/client?path=/statics/__webpack_hmr", join(process.cwd(), "client")],
-	output: {
-		path: join(process.cwd(), "app/public"),
-		publicPath:"/statics/",
-		chunkFilename: '[name].chunk.js',
-		filename: "bundle.js",
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new HtmlWebpackPlugin({
-			template: join(process.cwd(), "app/view/index.html"),
-		}),
-	],
-};
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports = {
+// 	// entry: resolve(__dirname, "./entry.js"),
+// 	entry: ["webpack-hot-middleware/client?path=/statics/__webpack_hmr", join(process.cwd(), "client")],
+// 	output: {
+// 		path: join(process.cwd(), "app/public"),
+// 		publicPath:"/statics/",
+// 		chunkFilename: '[name].chunk.js',
+// 		filename: "bundle.js",
+// 	},
+// 	plugins: [
+// 		new webpack.HotModuleReplacementPlugin(),
+// 		new HtmlWebpackPlugin({
+// 			template: join(process.cwd(), "app/view/index.html"),
+// 		}),
+// 	],
+// };
+
+module.exports = require(join(
+	__dirname,
+	`/webpack/webpack.${isDev ? "dev" : "prod"}.config.js`
+));
---
+++ src/webpack/webpack.base.config.js
@@ -0,0 +1,123 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const { join } = require("path");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const getCssLoaders = importLoaders => [
+	"style-loader",
+	{
+		loader: "css-loader",
+		options: {
+			modules: false,
+			sourceMap: isDev,
+			importLoaders,
+		},
+	},
+	{
+		loader: "postcss-loader",
+		options: {
+			ident: "postcss",
+			plugins: [
+				require("postcss-flexbugs-fixes"),
+				require("postcss-preset-env")({
+					autoprefixer: {
+						grid: true,
+						flexbox: "no-2009",
+					},
+					stage: 3,
+				}),
+				require("postcss-normalize"),
+			],
+			sourceMap: isDev,
+		},
+	},
+];
+
+module.exports = {
+	entry: isDev
+		? ["webpack-hot-middleware/client?path=/statics/__webpack_hmr", join(process.cwd(), "client")]
+		: join(process.cwd(), "client"),
+	output: {
+		path: join(process.cwd(), "app/public"),
+		publicPath: "/statics/",
+		chunkFilename: "[name].chunk.js",
+		filename: "bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(tsx?|js)$/,
+				loader: "babel-loader",
+				options: { cacheDirectory: true },
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: getCssLoaders(1),
+			},
+			{
+				test: /\.less$/,
+				use: [
+					...getCssLoaders(2),
+					{
+						loader: "less-loader",
+						options: {
+							sourceMap: isDev,
+						},
+					},
+				],
+			},
+			{
+				test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+				use: [
+					{
+						loader: "url-loader",
+						options: {
+							limit: 10 * 1024,
+							name: "[name].[contenthash:8].[ext]",
+							outputPath: "assets/images",
+						},
+					},
+				],
+			},
+			{
+				test: /\.(ttf|woff|woff2|eot|otf)$/,
+				use: [
+					{
+						loader: "url-loader",
+						options: {
+							name: "[name].[contenthash:8].[ext]",
+							outputPath: "assets/fonts",
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			template: join(process.cwd(), "app/view/index.html"),
+			filename: "index.html",
+			minify: isDev
+				? false
+				: {
+						removeAttributeQuotes: true,
+						collapseWhitespace: true,
+						removeComments: true,
+						collapseBooleanAttributes: true,
+						collapseInlineTagWhitespace: true,
+						removeRedundantAttributes: true,
+						removeScriptTypeAttributes: true,
+						removeStyleLinkTypeAttributes: true,
+						minifyCSS: true,
+						minifyJS: true,
+						minifyURLs: true,
+						useShortDoctype: true,
+				  },
+		}),
+	],
+};
